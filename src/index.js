e0d8a15fbbc4f9473466c17d4f7fda1ddb765509
ncaudill27 <nelson.a.caudill@gmail.com>
2020-03-01 20:50:31 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -12,4 +12,31 @@ document.addEventListener("DOMContentLoaded", () => {
       toyForm.style.display = "none";
     }
   });
+  getToys()
 });
+
+function getToys() {
+  fetch('http://localhost:3000/toys')
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(object) {
+      object.forEach(toy => {
+        const toyCollection = document.getElementById('toy-collection')
+        const toyDiv = document.createElement('div')
+        const toyName = document.createElement('h2')
+        const toyImg = document.createElement('img')
+        const toyLikes = document.createElement('p')
+        const likeBtn = document.createElement('button')
+        toyName.innerText = toy.name
+        toyImg.src = toy.image
+        toyLikes.innerText = `${toy.likes} Likes`
+        likeBtn.innerText = 'Like <3'
+        toyDiv.appendChild(toyName)
+        toyDiv.appendChild(toyImg)
+        toyDiv.appendChild(toyLikes)
+        toyDiv.appendChild(likeBtn)
+        toyCollection.appendChild(toyDiv)
+      })
+    })
+}
