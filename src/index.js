6368bb2af85afe60ec88cb8498026c265a54c8f5
ncaudill27 <nelson.a.caudill@gmail.com>
2020-03-01 21:56:46 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -22,29 +22,33 @@ function getToys() {
     })
     .then(function(object) {
       object.forEach(toy => {
-        const toyCollection = document.getElementById('toy-collection')
-        const toyDiv = document.createElement('div')
-        toyDiv.setAttribute('class', 'card')
-
-        const toyName = document.createElement('h2')
-        toyName.innerText = toy.name
-
-        const toyImg = document.createElement('img')
-        toyImg.src = toy.image
-        toyImg.setAttribute('class', 'toy-avatar')
-
-        const toyLikes = document.createElement('p')
-        toyLikes.innerText = `${toy.likes} Likes`
-        
-        const likeBtn = document.createElement('button')
-        likeBtn.innerText = 'Like <3'
-        likeBtn.setAttribute('class', 'like-btn')
-        
-        toyDiv.appendChild(toyName)
-        toyDiv.appendChild(toyImg)
-        toyDiv.appendChild(toyLikes)
-        toyDiv.appendChild(likeBtn)
-        toyCollection.appendChild(toyDiv)
+        renderToy(toy)
       })
     })
+}
+
+function renderToy(toy) {
+  const toyCollection = document.getElementById('toy-collection')
+  const toyDiv = document.createElement('div')
+  toyDiv.setAttribute('class', 'card')
+
+  const toyName = document.createElement('h2')
+  toyName.innerText = toy.name
+
+  const toyImg = document.createElement('img')
+  toyImg.src = toy.image
+  toyImg.setAttribute('class', 'toy-avatar')
+
+  const toyLikes = document.createElement('p')
+  toyLikes.innerText = `${toy.likes} Likes`
+  
+  const likeBtn = document.createElement('button')
+  likeBtn.innerText = 'Like <3'
+  likeBtn.setAttribute('class', 'like-btn')
+  
+  toyDiv.appendChild(toyName)
+  toyDiv.appendChild(toyImg)
+  toyDiv.appendChild(toyLikes)
+  toyDiv.appendChild(likeBtn)
+  toyCollection.appendChild(toyDiv)
 }
